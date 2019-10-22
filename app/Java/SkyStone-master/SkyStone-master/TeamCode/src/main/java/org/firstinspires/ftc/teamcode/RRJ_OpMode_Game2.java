dcd94878e04ab1bf54701f0c1e2a0c581c83af78
adviksundar <55121816+adviksundar@users.noreply.github.com>
2019-10-22 01:10:30 -0400

    modularized code for Nov 9th
    modularized code for Nov 9th

---
+++ app/Java/SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRJ_OpMode_Game2.java
@@ -0,0 +1,195 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="RRJ_OpMode_Game2", group="Linear Opmode")
+//@Disabled
+public class RRJ_OpMode_Game2 extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightBackDrive = null;
+    private Servo leftClawServo = null;
+    private Servo rightClawServo = null;
+    private DcMotor armLifter = null;
+
+    private double servoPosition = 0.0;
+    static final double INCREMENT = 0.1;     // amount to slew servo each CYCLE_MS cycle
+    static final int    CYCLE_MS  =  100;     // period of each cycle
+    static final double MAX_POS   =  0.4;     // Maximum rotational position
+    static final double MIN_POS   =  0.0;     // Minimum rotational position
+    double speedAdjust =7.0;
+    double  position = 0.0; //(MAX_POS - MIN_POS) / 2; // Start at halfway position
+    double armMotorPower=0.0;
+
+    @Override
+    public void runOpMode()
+    {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        initializeDriveMotor();
+        initializeServoMotor();
+        initializeArmMotor();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            driveBot();
+            driveClaws();
+            driveArm();
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Drive Motor Front", "left, right", leftFrontDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition());
+            telemetry.addData("Drive Motor Back", "left, right", leftBackDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
+            telemetry.addData("speedAdjust",  speedAdjust);
+            telemetry.addData("Gamepad1 button pressed", gamepad1.getRobocolMsgType());
+            telemetry.addData("ServoMotors", "left (%.2f), right (%.2f)", leftClawServo.getPosition(), rightClawServo.getPosition());
+            telemetry.update();
+
+        }
+    }
+
+    private void initializeArmMotor()
+    {
+        armLifter = hardwareMap.get(DcMotor.class, "ArmLifter");
+    }
+
+    private void driveArm()
+    {
+        if ( gamepad1.dpad_up )
+        {
+            armMotorPower = 0.2;
+        }
+        else if (gamepad1.dpad_down )
+        {
+            armMotorPower = -0.2;
+        }
+
+    }
+
+    private void initializeDriveMotor()
+    {
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "LeftFrontDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "RightFrontDrive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "LeftBackDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "RightBackDrive");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+    private void initializeServoMotor()
+    {
+        leftClawServo = hardwareMap.get(Servo.class, "LeftClaw");
+        rightClawServo = hardwareMap.get(Servo.class, "RightClaw");
+        leftClawServo.setDirection(Servo.Direction.FORWARD);
+        rightClawServo.setDirection(Servo.Direction.REVERSE);
+        leftClawServo.setPosition(position);
+        rightClawServo.setPosition(position);
+    }
+    private void driveBot()
+    {
+        if(gamepad1.dpad_left ==true)
+        {
+            speedAdjust -=1;
+        }
+        if(gamepad1.dpad_right ==true)
+        {
+            speedAdjust +=1;
+        }
+
+        leftBackDrive.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x)*(-speedAdjust/10));
+        rightBackDrive.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x)*(-speedAdjust/10));
+        leftFrontDrive.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x)*(-speedAdjust/10));
+        rightFrontDrive.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x)*(-speedAdjust/10));
+    }
+
+    private void  driveClaws()
+    {
+        // slew the servo, according to the rampUp (direction) variable.
+        if (position!=MAX_POS && gamepad1.y)
+        {
+            // Keep stepping up until we hit the max value.
+            position += INCREMENT ;
+            if (position >= MAX_POS )
+            {
+                position = MAX_POS;
+            }
+        }
+        else if (position != MIN_POS && gamepad1.a)
+        {
+            // Keep stepping down until we hit the min value.
+            position -= INCREMENT ;
+            if (position <= MIN_POS )
+            {
+                position = MIN_POS;
+
+            }
+        }
+        // Set the servo to the new position and pause;
+        leftClawServo.setPosition(position);
+        rightClawServo.setPosition(position);
+    }
+}
